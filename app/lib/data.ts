65fc756c9aee899592d3801d09652f277916cd6c
Vitor Scobin <vhscobin@gmail.com>
2024-01-02 22:16:33 -0300

    refact: remove timeouts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,7 +18,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     // console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -35,7 +34,6 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 5000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -59,7 +57,6 @@ export async function fetchCardData() {
   noStore();
 
   try {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
 
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
